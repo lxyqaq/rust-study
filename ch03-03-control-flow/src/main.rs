cd18cf1bfa88395c6546ab793908520b57b31da2
Xiangyu Liu <lxyqaq0128@gmail.com>
2022-03-02 15:16:28 +0000

    while and for

--- ch03-03-control-flow/src/main.rs
+++ ch03-03-control-flow/src/main.rs
@@ -20,4 +20,58 @@ fn main() {
         println!("number is not divisible by 4, 3, or 2");
     }
 
+    loop_function();
+
+    while_function();
+
+    for_function()
+
+}
+
+fn loop_function() {
+
+    let mut counter = 0;
+
+    let result = loop {
+
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+
+    };
+
+    println!("result = {}", result);
+
+}
+
+fn while_function() {
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+
+    println!("left");
+
+}
+
+fn for_function() {
+
+    let array = [10, 20, 30, 40, 50];
+
+    for element in array {
+        println!("value: {}", element);
+    }
+
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    // 反转数组
+    for i in (0..array.len()).rev() {
+        println!("value: {}", array[i]);
+    }
+
 }
