3882c4e085d1b8b8b676d2bcf768b1e8821d14c2
Xiangyu Liu <lxyqaq0128@gmail.com>
2022-03-07 16:50:22 +0000

    slice

--- ch04-03-slice/src/main.rs
+++ ch04-03-slice/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let str = String::from("Hello World");
+
+    // &str[0..5] = &str[..5]
+    let slice1 = &str[0..5];
+    println!("slice1 = {}", slice1);
+
+    // Hello
+    let slice2 = &str[..5];
+    println!("slice2 = {}", slice2);
+
+    // World
+    // &str[6..str.len()]
+    let slice3 = &str[6..];
+    println!("slice3 = {}", slice3);
+
+    // Hello World
+    let slice4 = &str[..];
+    print!("slice4 = {}", slice4);
+
 }
