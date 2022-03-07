f47f9a92586360c1400e1b3c667519c2c8761afa
Xiangyu Liu <lxyqaq0128@gmail.com>
2022-03-07 15:07:49 +0000

    reference

--- ch04-02-references-borrowing/src/main.rs
+++ ch04-02-references-borrowing/src/main.rs
@@ -9,8 +9,29 @@ fn main() {
 
     println!("The length of '{}' is {}.", s1, len);
 
+    // Mutable References
+    let mut s = String::from("Hello, World");
+
+    change(&mut s);
+
+    println!("s = {}", s);
+
+    // Mutable references have one big restriction:
+    // you can have only one mutable reference to a particular piece of data at a time.
+
+    let mut s2 = String::from("hello");
+
+    // first mutable borrow occurs here
+    let r1 = &mut s2;
+    let r2 = &mut s2;
+
+    println!("{}, {}", r1, r2);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(str: &mut String) {
+    str.push_str("+PLUS!!");
 }
